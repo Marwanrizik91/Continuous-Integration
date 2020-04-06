8b437ee010fb097274306fa4e5aa1848429c46c7
Jake <jakepowis@gmail.com>
2020-04-06 15:56:34 +0300

    jake breaking the tests!

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 function fizzbuzz(number) {
-    let rem3 = number % 3 == 0;
-    let rem5 = number % 5 == 0;
-     if (rem3 && rem5) {
-     return "Fizzbuzz"; // write your code here
-   } else if(rem5 ) {
-     return "Buzz"
-   } else if (rem3) {
-     return "Fizz"
-   } else if(typeof number != 'number') {
-     return "string"
-   } else {
-     return number
-   }
+  let rem3 = number % 3 == 0;
+  let rem5 = number % 5 == 0;
+  if (rem3 && rem5) {
+    return "Fizzbuzz"; // write your code here
+  } else if (rem5) {
+    return "Buz"
+  } else if (rem3) {
+    return "Fizz"
+  } else if (typeof number != 'number') {
+    return "string"
+  } else {
+    return number
   }
-  
+}
+
 
 
 
